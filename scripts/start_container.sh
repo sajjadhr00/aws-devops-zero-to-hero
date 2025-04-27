4ff9c8ae11a5f5f2064be6754917f45e802b8870
sajjadhr00 <141516491+sajjadhr00@users.noreply.github.com>
2025-04-27 14:47:04 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sajjadhr194/simple-pythoooooon-flask-app
+docker pull sajjadhr194/simple-pythoooooon-flask-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 sajjadhr194/simple-pythoooooon-flask-app
